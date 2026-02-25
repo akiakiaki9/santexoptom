89808589207d9409aa66044c402324174af0181d
Akbar <tillayevakbarshoh@gmail.com>
2026-02-25 19:26:25 +0500

    first commit

---
+++ src/app/utils/data.js
@@ -0,0 +1,126 @@
+export const DATA = [
+    {
+        id: 1,
+        name: 'Душевая кабина',
+        model: '804',
+        size: '100/100',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 2,
+        name: 'Душевая кабина',
+        model: '807',
+        size: '100/100',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 3,
+        name: 'Душевая кабина',
+        model: '805',
+        size: '100/100',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 4,
+        name: 'Душевая кабина',
+        model: '809',
+        size: '120/85',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 5,
+        name: 'Душевая кабина',
+        model: '806XB',
+        size: '100/100',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 6,
+        name: 'Душевая кабина',
+        model: '806SG',
+        size: '100/100',
+        image: '',
+        image_2: ''
+    },
+    //// 2
+    {
+        id: 7,
+        name: 'Душевая кабина',
+        model: '806H-L',
+        size: '120/120',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 8,
+        name: 'Душевая кабина',
+        model: '806H-H',
+        size: '135/135',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 9,
+        name: 'Душевая кабина',
+        model: '806XB',
+        size: '90/90',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 10,
+        name: 'Душевая кабина',
+        model: '806H-H',
+        size: '120/120',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 11,
+        name: 'Душевая кабина',
+        model: '805',
+        size: '90/90',
+        image: '',
+        image_2: ''
+    },
+    // tg
+    {
+        id: 12,
+        name: 'UMXIK CABIN',
+        model: 'UX-R5090B (40) Глубокий поддон',
+        size: '90×90×215',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 13,
+        name: 'UMXIK CABIN',
+        model: 'UX-R5090B (25) Средний поддон',
+        size: '90×90×215',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 14,
+        name: 'UMXIK CABIN',
+        model: 'UX-R5090B (13) Низкий поддон',
+        size: '90×90×215',
+        image: '',
+        image_2: ''
+    },
+    {
+        id: 15,
+        name: 'Душевая кабина',
+        model: '804',
+        size: '100/100',
+        image: '',
+        image_2: ''
+    },
+];
+
+export default DATA;
